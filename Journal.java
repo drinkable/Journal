7d518371d43459400dc8e5b08b2e11b74339d3c8
drinkable <jugalamin17@gmail.com>
2020-06-08 18:32:22 -0400

    added the variables and constructors for Journal

--- Journal.java
+++ Journal.java
@@ -1,3 +1,36 @@
+import java.io.File;
+import java.util.Date;
+
 public class Journal {
+
+    /**
+     * Elements of a journal entry. The body is strored in a file.
+     */
+
+    private File entry;
+    private String name;
+    private String body;
+    private Date date;
+
+    /**
+     * CONSTRUCTORS
+     */
+
+    Journal() {
+
+    }
+
+    Journal(String name) {
+        this.name = name;
+    }
+
+    Journal(String name, Date date){
+        this.name = name;
+        this.date = date;
+    }
+
+    Journal(Date date){
+        this.date = date;
+    }
 
 }
