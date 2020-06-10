3b7b40d507236407e6e1e9a13f4fe0d0df7f1749
drinkable <jugalamin17@gmail.com>
2020-06-10 19:25:42 -0400

    trying to create GUI with a builder.

--- GUI.java
+++ GUI.java
@@ -1,2 +1,20 @@
+import javax.swing.*;
+
 public class GUI {
+    private JTextField textField1;
+    private JComboBox comboBox1;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("GUI");
+        frame.setContentPane(new GUI().panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private JPanel panel;
 }
